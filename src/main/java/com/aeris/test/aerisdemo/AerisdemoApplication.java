1a61a81b92663b4ddd7ada0983b55303c995f215
Daniel Carda <GoneBy2025@CardaTechnologies.com>
2023-08-20 11:42:58 -0600

    Improved JavaDocs.

--- src/main/java/com/aeris/test/aerisdemo/AerisdemoApplication.java
+++ src/main/java/com/aeris/test/aerisdemo/AerisdemoApplication.java
@@ -1,13 +1,49 @@
+/* ---------------------------------------------------------------------------------------
+ * Class:  com.aeris.test.aerisdemo.AerisdemoApplication.java
+ * Date:   2023/08/20
+ * ---------------------------------------------------------------------------------------
+ *
+ */
+
 package com.aeris.test.aerisdemo;
+
+//~--- non-JDK imports --------------------------------------------------------
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Package:    com.aeris.test.aerisdemo
+ * Class:      AerisdemoApplication.java
+ * Desc:       ...
+ *
+ * @author     Daniel Carda
+ *
+ * Maintenance History:
+ * <p><pre>
+ * yyyy mm dd  Who               Description
+ * ----------  ----------------  ----------------------------------------------------
+ * 2023/08/20  JavaDan           Initial Module Creation...
+ * </pre>
+ *
+ */
 @SpringBootApplication
 public class AerisdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AerisdemoApplication.class, args);
-	}
-
+    /**
+     * Method: main
+     *
+     * @param args
+     *         Param Desc...
+     */
+    public static void main( String[] args ) {
+        SpringApplication.run( AerisdemoApplication.class, args );
+    }
 }
+
+/* ---------------------------------------------------------------------------------------
+ *
+ * ---------------------------------------------------------------------------------------
+ * Class:  com.aeris.test.aerisdemo.AerisdemoApplication.java
+ * Date:   2023/08/20
+ * --------------------------------------------------------------------------------------- */
