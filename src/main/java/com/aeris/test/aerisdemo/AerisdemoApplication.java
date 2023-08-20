5e6e877eead60cf62fd68f64b1bbf8449624df5b
Daniel Carda <GoneBy2025@CardaTechnologies.com>
2023-08-20 11:40:59 -0600

    Initial Load. This establishes the base of the project. I generated this using spring.io (because oh why not! :-) )

---
+++ src/main/java/com/aeris/test/aerisdemo/AerisdemoApplication.java
@@ -0,0 +1,13 @@
+package com.aeris.test.aerisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerisdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerisdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeris/test/aerisdemo/AerisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeris.test.aerisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerisdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
