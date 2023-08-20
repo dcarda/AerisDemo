9f6bf02c69c7ae48d0485077259b5ecc429eff72
Daniel Carda <GoneBy2025@CardaTechnologies.com>
2023-08-20 12:56:13 -0600

    Got the REST framework working.  Need to improve.

--- src/main/java/com/aeris/test/aerisdemo/controller/LandingPadController.java
+++ src/main/java/com/aeris/test/aerisdemo/controller/LandingPadController.java
@@ -1,4 +1,44 @@
 package com.aeris.test.aerisdemo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class LandingPadController {
+
+
+    @RequestMapping("/get-data")
+    public String serviceGetData() {
+
+        String responseStr;
+        responseStr = " LandingPadController : get-data ";
+        // Return
+
+        return (responseStr);
+    }
+
+
+    @RequestMapping("/get-image")
+    public String serviceGetImage() {
+
+        String responseStr;
+        responseStr = " LandingPadController : get-image ";
+        System.out.println(responseStr);
+
+        return (responseStr);
+    }
+
+    @RequestMapping("/get-info")
+    public String serviceGetInfo() {
+
+        String responseStr;
+
+        responseStr = " LandingPadController : get-info ";
+        System.out.println(responseStr);
+
+        return (responseStr);
+    }
+
+
 }
