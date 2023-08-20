13551c9d06a683bb8d73c4d1b4a82a057a39a4d9
Daniel Carda <GoneBy2025@CardaTechnologies.com>
2023-08-20 11:45:00 -0600

    This is a file I use to set my command line environment. I find every project has it's own JDK / build parameters.

---
+++ setEnv.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+clear
+
+export JAVA_HOME=/d/Tools/Java/JDK/jdk-11.0.11
+export ANT_HOME=/d/Tools/Java/Apache/apache-ant-1.10.11
+export MAVEN_HOME=/d/Tools/Java/Apache/apache-maven-3.9.0
+
+export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$MAVEN_HOME/bin:$PATH
+
+# Update the Path
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Display Information
+echo --  BASH   ----------------------------------------------------------
+which bash
+bash -version
+echo
+echo --  JAVA   ----------------------------------------------------------
+which java
+java -version
+echo
+echo --  MAVEN  ----------------------------------------------------------
+which mvn
+mvn -version
+echo
+echo --  ANT    ----------------------------------------------------------
+which ant
+ant -version
+echo
+
+
